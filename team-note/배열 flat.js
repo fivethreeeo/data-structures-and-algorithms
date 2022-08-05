d7ea692f85b8c23bb87914e6a9114cf9e7df5cd5
Sangbeom Heo <mansaout@gmail.com>
2022-08-05 13:31:40 +0900

    add team-note

---
+++ team-note/배열 flat.js
@@ -0,0 +1,6 @@
+function flatten(arr) {
+  return arr.reduce(
+    (acc, cur) => acc.concat(Array.isArray(cur) ? flatten(cur) : cur),
+    []
+  );
+}
---
+++ team-note/배열 내 중복 제거.js
@@ -0,0 +1,3 @@
+const names = ["Lee", "Kim", "Park", "Lee", "Kim"];
+const uniqueNamesWithArrayFrom = Array.from(new Set(names));
+const uniqueNamesWithSpread = [...new Set(names)];
