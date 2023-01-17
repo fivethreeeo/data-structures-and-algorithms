bcc9ded85ab31a615de7bb9cdfe6f09655e16db7
Sangbeom Heo <mansaout@gmail.com>
2023-01-18 02:20:01 +0900

    Programmers / K번째수 / Level 1 / 00:05:12
    https://programmers.co.kr/learn/courses/30/lessons/42748

--- solutions/Programmers/42748_K번째수.js
+++ solutions/Programmers/42748_K번째수.js
@@ -1,10 +1,13 @@
-// https://programmers.co.kr/learn/courses/30/lessons/42748
+/*
 
+    Programmers / K번째수 / Level 1 / 00:05:12
+    https://programmers.co.kr/learn/courses/30/lessons/42748
+
+*/
 function solution(array, commands) {
-  return commands.map((command) => {
-    const [i, j, k] = command;
-    return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
-  });
+  return commands.map(
+    ([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
+  );
 }
 
 console.log(
